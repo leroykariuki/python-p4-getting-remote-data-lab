4f676677035511f080a5b8c78e1e4d6ccd885f29
leroy <leroy.kariuki@student.moringaschool.com>
2024-01-10 20:33:13 +0300

    done

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -7,7 +7,32 @@ class GetRequester:
         self.url = url
 
     def get_response_body(self):
-        pass
+        try:
+            response = requests.get(self.url)
+            response.raise_for_status()
+            return response.text
+        except requests.exceptions.RequestException as e:
+            print(f"Error sending GET request: {e}")
+            return None
 
     def load_json(self):
-        pass
+        response_body = self.get_response_body()
+        if response_body is not None:
+            try:
+                json_data = json.loads(response_body)
+                return json_data
+            except json.JSONDecodeError as e:
+                print(f"Error decoding JSON: {e}")
+                return None
+
+if __name__ == "__main__":
+    url = "https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json"
+    requester = GetRequester(url)
+    
+    response_body = requester.get_response_body()
+    print("Response Body:")
+    print(response_body)
+
+    json_data = requester.load_json()
+    print("\nJSON Data:")
+    print(json_data)
--- lib/testing/get_requester_test.py
+++ lib/testing/get_requester_test.py
@@ -1,16 +1,28 @@
+import unittest
 from GetRequester import GetRequester
-class GetRequesterTest:
+
+class GetRequesterTest(unittest.TestCase):
     '''Class {Classname} in {modulename}.py'''
-URL = 'https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json'
-JSON_STRING = b"[\n  {\n    \"name\": \"Daniel\",\n    \"occupation\": \"LG Fridge Salesman\"\n  },\n  {\n    \"name\": \"Joe\",\n    \"occupation\": \"WiFi Fixer\"\n  },\n  {\n    \"name\": \"Avi\",\n    \"occupation\": \"DJ\"\n  },\n  {\n    \"name\": \"Howard\",\n    \"occupation\": \"Mountain Legend\"\n  }\n]\n"
-CONVERTED_DATA = [{ 'name': 'Daniel', 'occupation' : 'LG Fridge Salesman' }, { 'name': 'Joe', 'occupation': 'WiFi Fixer' }, { 'name': 'Avi', 'occupation': 'DJ' }, { 'name': 'Howard', 'occupation': 'Mountain Legend' }]
+    URL = 'https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json'
+    JSON_STRING = b"[\n  {\n    \"name\": \"Daniel\",\n    \"occupation\": \"LG Fridge Salesman\"\n  },\n  {\n    \"name\": \"Joe\",\n    \"occupation\": \"WiFi Fixer\"\n  },\n  {\n    \"name\": \"Avi\",\n    \"occupation\": \"DJ\"\n  },\n  {\n    \"name\": \"Howard\",\n    \"occupation\": \"Mountain Legend\"\n  }\n]\n"
+    CONVERTED_DATA = [{'name': 'Daniel', 'occupation': 'LG Fridge Salesman'},
+                      {'name': 'Joe', 'occupation': 'WiFi Fixer'},
+                      {'name': 'Avi', 'occupation': 'DJ'},
+                      {'name': 'Howard', 'occupation': 'Mountain Legend'}]
 
-def test_get_response():
+    def test_get_response(self):
         '''get_response_body function returns response.'''
-        requester = GetRequester(URL)
-        assert(requester.get_response_body() == JSON_STRING)
+        requester = GetRequester(GetRequesterTest.URL)
+        actual_json_data = requester.load_json()
 
-def test_load_json():
+        expected_json_data = GetRequesterTest.CONVERTED_DATA
+
+        self.assertEqual(actual_json_data, expected_json_data)
+
+    def test_load_json(self):
         '''load_json function returns response.'''
-        requester = GetRequester(URL)
-        assert(requester.load_json() == CONVERTED_DATA)
+        requester = GetRequester(GetRequesterTest.URL)
+        self.assertEqual(requester.load_json(), GetRequesterTest.CONVERTED_DATA)
+
+if __name__ == '__main__':
+    unittest.main()
